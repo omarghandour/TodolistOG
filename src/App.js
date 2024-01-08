16f28b1fb0e3aba8b5826f7030eb5bd8d6bdb541
omarghandour <alex.forget50@gmail.com>
2024-01-09 00:14:25 +0200

    env

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import Todo from './pages/Todo'
-import M from './components/M'
-import { Helmet } from 'react-helmet-async'
+import React from "react";
+import Todo from "./pages/Todo";
+import M from "./components/M";
+import { Helmet } from "react-helmet-async";
 // import firebase from './firebase'
 const App = () => {
   // React.useEffect(()=>{
@@ -14,21 +14,27 @@ const App = () => {
   // })
 
   return (
-    <div className='app'>
+    <div className="app">
       <Helmet>
         <title>Todo</title>
-        <meta name='application-name' content='OGTodo' />
-        <meta name='author' content='Omar Ghandour' />
-        <meta name='description' content='OGTodo is a smart and
+        <meta name="application-name" content="OGTodo" />
+        <meta name="author" content="Omar Ghandour" />
+        <meta
+          name="description"
+          content="OGTodo is a smart and
                             intuitive app that lets you create, edit, and complete tasks
-                            with just a few taps.' />
-        <meta name='keywords' content='Todo list, Todolist, TodoList, React, Ghandour, List, Todo, tasks' />
+                            with just a few taps."
+        />
+        <meta
+          name="keywords"
+          content="Todo list, Todolist, TodoList, React, Ghandour, List, Todo, tasks"
+        />
       </Helmet>
       <Todo />
       <M />
       {/* <p className='copyright'>©️ Omar Ghandour</p> */}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
